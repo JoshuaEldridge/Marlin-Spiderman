24f638f5f3152438510d786e134d3efc5ad71166
Joshua Eldridge <joshuae74@hotmail.com>
2017-12-17 09:39:19 -0500

    Updating Configuration file to correct for bed center. Removing previous updates to SanityCheck.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -800,14 +800,13 @@
 // @section machine
 
 // The size of the print bed
-//#define X_BED_SIZE 200
-//#define Y_BED_SIZE 200
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 200
+#define Y_BED_SIZE 200
+
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 10
-#define Y_MIN_POS -10
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1040,8 +1039,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS 10
-#define MANUAL_Y_HOME_POS -10
+#define MANUAL_X_HOME_POS 0
+#define MANUAL_Y_HOME_POS -20
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
